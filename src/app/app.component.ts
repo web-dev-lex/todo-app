1f6ced082d6e1a5c6494973a8ca6626a4b7faf1b
Lex <webdevlex35@gmail.com>
2022-08-06 15:34:21 -0400

    created item interface, items array and get method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    { description: 'eat', done: true },
+    { description: 'sleep', done: false },
+    { description: 'play', done: false },
+    { description: 'laugh', done: false },
+  ];
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) => this.filter 
+    === 'done' ? item.done : !item.done);
+  }
 }
---
+++ src/app/item.ts
@@ -0,0 +1,6 @@
+
+
+export interface Item {
+  description: string;
+  done: boolean;
+}
